27a8c9b7ccfb3d6344fba167199bade12cb46f1b
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-12-15 21:34:35 -0800

    setting up plants router

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const Plant = require('../plants/plants-model')
 
-
-
+router.get('/', (req, res, next) => {
+    res.status(200).json({message: 'plants router connected'})
+})
 
 
 module.exports = router 
