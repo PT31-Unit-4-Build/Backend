5b5ab27f0e518a13c4f5d46d4d1b57c6a4a1e038
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-12-22 21:38:13 -0800

    heroku push

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,43 +1,43 @@
 exports.up = async (knex) => {
-  await knex.schema
-    .createTable('users', (tbl) => {
-      tbl.increments()
-      tbl.string('username', 200).notNullable().unique()
-      tbl.string('password', 200).notNullable()
-      tbl.string('phoneNumber', 10).notNullable().unique()
-      tbl.timestamps(false, true)
-    })
+    await knex.schema
+        .createTable('users', (tbl) => {
+            tbl.increments()
+            tbl.string('username', 200).notNullable().unique()
+            tbl.string('password', 200).notNullable()
+            tbl.string('phoneNumber', 10).notNullable().unique()
+            tbl.timestamps(false, true)
+        })
 
-    .createTable('plants', (tbl) => {
-      tbl.increments()
-      tbl.string('nickname')
-      tbl.string('species')
-      tbl.string('h2o_frequency')
-      tbl.string('image')
-    })
-    .createTable('users_plants', (tbl) => {
-      tbl.increments()
-      tbl.integer('user_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-      tbl.integer('plant_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('plants')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-    })
+        .createTable('plants', (tbl) => {
+            tbl.increments()
+            tbl.string('nickname')
+            tbl.string('species')
+            tbl.string('h2o_frequency')
+            tbl.string('image')
+        })
+        .createTable('users_plants', (tbl) => {
+            tbl.increments()
+            tbl.integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')
+            tbl.integer('plant_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('plants')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE')
+        })
 
 }
 exports.down = function (knex) {
 
-  return knex.schema
-    .dropTableIfExists('users_plants')
-    .dropTableIfExists('plants')
-    .dropTableIfExists('users')
+    return knex.schema
+        .dropTableIfExists('users_plants')
+        .dropTableIfExists('plants')
+        .dropTableIfExists('users')
 }
